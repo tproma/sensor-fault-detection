109ce66057ddea8c7542880f86c1ec9b53574aed
Tanjina <tanjinaproma@gnail.com>
2023-02-13 00:15:10 +0600

    update mongodb_connection.py

--- sensor/configuration/mongo_db_connection.py
+++ sensor/configuration/mongo_db_connection.py
@@ -10,8 +10,8 @@ class MongoDBClient:
     def __init__(self, database_name=DATABASE_NAME) -> None:
         try:
             if MongoDBClient.client is None:
-                #mongo_db_url = os.getenv(MONGODB_URL_KEY)
-                mongo_db_url =" "
+                mongo_db_url = os.getenv(MONGODB_URL_KEY)
+                #mongo_db_url =" "
                 MongoDBClient.client = pymongo.MongoClient(mongo_db_url, tlsCAFile = ca)
             self.client = MongoDBClient.client
             self.database = self.client[database_name]
